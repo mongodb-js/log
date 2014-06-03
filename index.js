78ae99b3c4139bbcc2204c913aa9ba1d8a5b471b
Waley Chen <waleycz@gmail.com>
2014-06-03 02:00:07 -0400

    style changes - indentation & breaking lines into multiple lines

--- index.js
+++ index.js
@@ -2,49 +2,60 @@ var debug = require('debug')('mongolog'),
     regret = require('./regretSetup.js'),
     util = require('util');
 
-function errorMessage(msg){
-  if(msg.indexOf('mongod instance already running?') > -1){
-  return new Error('already running');
-  }
+function errorMessage(msg) {
+  if (msg.indexOf('mongod instance already running?') > -1)
+    return new Error('already running');
+
   return new Error(msg);
 }
 
-function getEvent(msg){
-  if(msg.indexOf('exception') > -1){
-    return {name: 'error', data: errorMessage(msg)};
-  }
-  else if(msg.indexOf('waiting for connections') > -1){
-    return {name: 'ready', data: {port: parseInt(/(\d+)/.exec(msg)[1], 10)}};
-  }
+function getEvent(msg) {
+  if (msg.indexOf('exception') > -1)
+    return { name: 'error', data: errorMessage(msg) };
+  else if(msg.indexOf('waiting for connections') > -1)
+    return {
+      name: 'ready', 
+      data: {port: parseInt(/(\d+)/.exec(msg)[1], 10)} 
+    };
+
   return null;
 }
 
-function Entry(data, opts){
-  opts = opts || {};
+function Entry(data, opts) {
   data = data || {};
-  opts.wrap = opts.wrap || 80;
-  this.name = data.name;
-  this.message = data.message || '';
+  opts = opts || {};
+
   this.date = data.date || new Date();
+  this.name = data.name;
+
+  opts.wrap = opts.wrap || 80;
+
+  this.message = data.message || '';
   this.event = getEvent(this.message);
 }
 
-Entry.prototype.toString = function(){
+Entry.prototype.toString = function() {
   return this.date.toString() + ' [' + this.name + '] ' + this.message;
 };
 
-module.exports.parse = function(lines, opts){
+module.exports.parse = function(lines, opts) {
   opts = opts || {};
-  if(!Array.isArray(lines)){
-    lines = [lines];
-  }
-  return lines.filter(function(line){
-    return line && line.length > 0;
-  }).map(function(line){
-    var match = regret(/^mongodb.log/, line, opts);
-    if(!match){
-      match = {message: line};
+
+  if (!Array.isArray(lines))
+    lines = [ lines ];
+
+  return lines.filter(
+    function(line) {
+      return line && line.length > 0;
     }
-    return new Entry(match);
-  });
+  ).map(
+    function(line) {
+      var match = regret(/^mongodb.log/, line, opts);
+
+      if (!match)
+        match = { message: line };
+
+      return new Entry(match);
+    }
+  );
 };
