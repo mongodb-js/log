bf78d05416c437d3e2fadc31a072a72e974d8996
Waley Chen <waleycz@gmail.com>
2014-06-03 01:35:47 -0400

    separate setup of regret into it's own module

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-var regret = require('regret'),
-  util = require('util'),
-  debug = require('debug')('mongolog');
-
-regret.add('date.iso',
-  /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{1,3}-\d{4}/,
-  '2014-02-13T18:00:04.709-0500');
-
-regret.add('date',
-  /\w{3} \w{3} \d{1,2} \d{1,2}:\d{2}:\d{2}/,
-  'Wed Mar 12 14:42:31');
-
-regret.add('mongodb.log', /({{date.iso}}|{{date}}) \[(\w+)\] (.*)/,
-  '2014-02-13T18:00:04.709-0500 [initandlisten] db version v2.5.6-pre-',
-  ['date', 'name', 'message']);
-
-regret.add('mongodb.logshutdown', /(\w+)\: (.*)/,
-  'dbexit: really exiting now', ['name', 'message']);
+var debug = require('debug')('mongolog'),
+    regret = require('./regretSetup.js'),
+    util = require('util');
 
 function errorMessage(msg){
   if(msg.indexOf('mongod instance already running?') > -1){
