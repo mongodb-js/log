45651fbda03cb3da3521012352f76bc6e4c26f3c
Waley Chen <waleycz@gmail.com>
2014-06-05 18:20:54 -0400

    separate regret setup

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-var regret = require('regret');
-
-regret.add('date.iso',
-  /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{1,3}-\d{4}/,
-  '2014-02-13T18:00:04.709-0500');
-
-regret.add('date',
-  /\w{3} \w{3} \d{1,2} \d{1,2}:\d{2}:\d{2}/,
-  'Wed Mar 12 14:42:31');
-
-regret.add('mongodb.log', /({{date.iso}}|{{date}}) \[(\w+)\] (.*)/,
-  '2014-02-13T18:00:04.709-0500 [initandlisten] db version v2.5.6-pre-',
-  ['date', 'name', 'message']);
-
-regret.add('mongodb.logshutdown', /(\w+)\: (.*)/,
-  'dbexit: really exiting now', ['name', 'message']);
+var regret = require('./regretSetup');
 
 function errorMessage(msg){
   if(msg.indexOf('mongod instance already running?') > -1){
