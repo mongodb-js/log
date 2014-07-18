fafbc6eafe79ef3036780cb442ea4dfc4ce16e55
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-07-18 15:34:43 -0400

    remove extraneous deps and warn on unhandled cases instead of causing an undefined crash

--- index.js
+++ index.js
@@ -1,62 +1,89 @@
-var debug     = require('debug')('index'),
-    regret    = require('./patterns'),
-    Set       = require('set');
+var debug = require('debug')('mongodb-log'),
+  regret = require('./patterns');
+
+
+var OPS = 'command delete getmore query remove update'.split(' ');
+var QUERY_OPS = '$exists $gt $gte $in $lt $lte $nin $regex'.split(' ');
+
+function getTimestampFormat(timestamp){
+  var format = undefined,
+    l = timestamp.length;
+
+  switch(l){
+    case 19:
+      format = 'ctime-pre2.4';
+      break;
+    case 23:
+      format = 'ctime';
+      break;
+    case 24:
+      format = 'iso8601-utc';
+      break;
+    case 28:
+      format = 'iso8601-local';
+      break;
+  }
+  return format;
+}
 
 function errorMessage(msg){
+  var message = msg;
+
   if(msg.indexOf('mongod instance already running?') > -1){
-    return new Error('already running');
+    message = 'already running';
   }
-  return new Error(msg);
+
+  return new Error(message);
 }
 
 function getEvent(msg){
-  if (msg.indexOf('exception') > -1) {
-    return {name: 'error', data: errorMessage(msg)};
-  } else if(msg.indexOf('waiting for connections') > -1) {
-    return {name: 'ready', data: {port: parseInt(/(\d+)/.exec(msg)[1], 10)}};
+  if(msg.indexOf('exception') > -1){
+    return {
+      name: 'error',
+      data: errorMessage(msg)
+    };
   }
+
+  if(msg.indexOf('waiting for connections') > -1) {
+    return {
+      name: 'ready',
+      data: {port: parseInt(/(\d+)/.exec(msg)[1], 10)}
+    };
+  }
+
   return null;
 }
-
-var operationTypes = new Set([
-  'command', 
-  'delete', 
-  'getmore', 
-  'query', 
-  'remove', 
-  'update' 
-]);
 
 function Entry(data, opts){
   opts = opts || {};
   data = data || {};
 
-  opts.wrap = opts.wrap || 80;
+  this.timestamp = data.timestamp || new Date();
+  this.timestamp_format = getTimestampFormat(this.timestamp);
 
-  // general fields
-  parseTimestampFields(this, data.timestamp);
-
-  this.event = getEvent(data.message);
   this.line = data.line;
   this.message = data.message || '';
-  this.tokens = data.line.split(' ');
+  this.tokens = data.line ? data.line.split(' ') : [];
   this.thread = data.thread;
 
+  this.event = getEvent(data.message);
+
   var match;
-
   // connection accepted format
-  match = regret('connectionAccepted', data.message);
-
-  if (match !== null)
+  if((match = regret('connectionAccepted', data.message))){
     this.conn = 'conn' + match.connNum;
-  else if (data.thread.substring(0, 4) === 'conn')
+  }
+  else if(data.thread && data.thread.substring(0, 4) === 'conn'){
     this.conn = data.thread;
+  }
+
+  if(!data.line || !data.thread) console.warn('suspicious entry', data);
 
   // operation format
   // the operation type comes after the thread
   var opTypeIndex = this.tokens.indexOf('[' + this.thread + ']') + 1;
 
-  if (operationTypes.contains(this.tokens[opTypeIndex])) {
+  if(OPS.indexOf(this.tokens[opTypeIndex]) > -1){
     var lastToken = this.tokens.slice(-1)[0];
     this.duration = parseInt(lastToken.substring(0, lastToken.length - 2));
     this.operation = this.tokens[2];
@@ -65,7 +92,7 @@ function Entry(data, opts){
     // opTypeIndex + 2 is where the query object should start
     parseObject('sort_shape', 'orderby:', this, opTypeIndex + 2, false);
 
-    var tokensIndex = parseObject('query', 'query:', this, opTypeIndex + 2, 
+    var tokensIndex = parseObject('query', 'query:', this, opTypeIndex + 2,
       false);
     parseQueryShape(this);
 
@@ -100,11 +127,11 @@ function parseOperationStats(thisObj, token) {
     intValue = parseInt(token.substring(colonIndex + 1));
 
     if (!isNaN(intValue))
-      thisObj[key] = intValue; 
+      thisObj[key] = intValue;
   }
 }
 
-function parseObject(objectName, objectToken, thisObj, tokensIndex, 
+function parseObject(objectName, objectToken, thisObj, tokensIndex,
     parsedFirstNestedObj) {
   if (thisObj.tokens.indexOf(objectToken) === -1)
     return tokensIndex;
@@ -115,13 +142,13 @@ function parseObject(objectName, objectToken, thisObj, tokensIndex,
   debug();
 
   tokensIndex++;
-  parsedFirstNestedObj = (typeof parsedFirstNestedObj !== 'undefined') ? 
+  parsedFirstNestedObj = (typeof parsedFirstNestedObj !== 'undefined') ?
     parsedFirstNestedObj : false;
 
   var leftParenCount = 0, rightParenCount = 0;
-  var parsingArray = false, 
-      parsingRegex = false, 
-      parsingSingleQuotedString = false, 
+  var parsingArray = false,
+      parsingRegex = false,
+      parsingSingleQuotedString = false,
       parsingDoubleQuotedString = false;
   var objectStartIndex = tokensIndex;
   var JSONtokens = thisObj.tokens.slice(0);
@@ -148,10 +175,10 @@ function parseObject(objectName, objectToken, thisObj, tokensIndex,
       if (token.search('/') >= 0)
         parsingRegex = false;
     } else if (parsingSingleQuotedString) {
-      if (token.search('\'') >= 0) 
+      if (token.search('\'') >= 0)
         parsingSingleQuotedString = false;
     } else if (parsingDoubleQuotedString) {
-      if (token.search('\"') >= 0) 
+      if (token.search('\"') >= 0)
         parsingDoubleQuotedString = false;
     } else if (tokenBeginsWrap(token, '/')) {
       parsingRegex = true;
@@ -161,7 +188,7 @@ function parseObject(objectName, objectToken, thisObj, tokensIndex,
       parsingDoubleQuotedString = true;
     } else if (tokenBeginsWrap(token, '[')) {
       parsingArray = true;
-    } 
+    }
 
     // handles the nested objectToken, see test cases
     else if (!parsedFirstNestedObj && token === objectToken &&
@@ -176,12 +203,12 @@ function parseObject(objectName, objectToken, thisObj, tokensIndex,
       leftParenCount++;
     } else if (token === '}' || token === '},') {
       rightParenCount++;
-    } else if (objectToken === 'query:' && token.length > 1 && 
+    } else if (objectToken === 'query:' && token.length > 1 &&
         token.slice(-1) === ':') {
       JSONtokens[tokensIndex] = '\"' + token.slice(0, -1) + '\":';
     }
 
-    debug('leftParenCount: ' + leftParenCount + ' rightParenCount: ' + 
+    debug('leftParenCount: ' + leftParenCount + ' rightParenCount: ' +
     rightParenCount);
     debug('parsingRegex: ' + parsingRegex);
     debug('parsingSingleQuotedString: ' + parsingSingleQuotedString);
@@ -212,14 +239,14 @@ function parseObject(objectName, objectToken, thisObj, tokensIndex,
 }
 
 function tokenBeginsWrap(token, wrapSymbol) {
-  return token === wrapSymbol || 
-      (token[0] === wrapSymbol && 
-      token.slice(-1) !== wrapSymbol && 
+  return token === wrapSymbol ||
+      (token[0] === wrapSymbol &&
+      token.slice(-1) !== wrapSymbol &&
       token.slice(-2) !== wrapSymbol + ',');
 }
 
 function parseQueryShape(thisObj) {
-  if (thisObj.query === undefined) 
+  if (thisObj.query === undefined)
     return;
 
   var queryObject = eval('(' + thisObj.query + ')'),
@@ -232,26 +259,15 @@ function parseQueryShape(thisObj) {
   thisObj.queryShape = queryShape;
 }
 
-var queryOperators = new Set([
-  '$exists',
-  '$gt', 
-  '$gte', 
-  '$in', 
-  '$lt', 
-  '$lte',
-  '$nin',
-  '$regex'
-]);
-
 function parseQueryShapeObject(obj) {
   var value;
 
   for (var key in obj) {
     value = obj[key];
 
-    if (queryOperators.contains(key))
-      return 1;
-    else if (Array.isArray(value))
+    if(QUERY_OPS.indexOf(key) > -1) return 1;
+
+    if(Array.isArray(value))
       obj[key] = parseQueryShapeArray(value);
     else if (typeof(value) === 'object' && !(value instanceof RegExp))
       obj[key] = parseQueryShapeObject(value);
@@ -275,21 +291,6 @@ function parseQueryShapeArray(ary) {
   }
 
   return ary.sort();
-}
-
-var timestampLengths = {
-  '19': 'ctime-pre2.4',
-  '23': 'ctime',
-  '24': 'iso8601-utc',
-  '28': 'iso8601-local'
-};
-
-function parseTimestampFields(thisObj, timestamp) {
-  thisObj.timestamp = timestamp || new Date();
-  var tsLength = thisObj.timestamp.length;
-
-  if (timestampLengths[tsLength] !== undefined)
-    thisObj.timestamp_format = timestampLengths[tsLength];
 }
 
 module.exports.parse = function(lines, opts){
