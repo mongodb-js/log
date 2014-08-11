df03ed2ce716519c46906f59819c9b2c6ea2586f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-08-11 14:12:17 -0400

    formats you codes automatically

--- index.js
+++ index.js
@@ -1,174 +1,128 @@
-var JSONL   = require('json-literal')
-    debug   = require('debug')('mongodb-log'),
-    regret  = require('./patterns');
-
-
+var JSONL = require('json-literal'), debug = require('debug')('mongodb-log'), regret = require('./patterns');
 var OPS = 'command delete getmore query remove update'.split(' ');
 var QUERY_OPS = '$exists $gt $gte $in $lt $lte $nin $regex'.split(' ');
-
-function getTimestampFormat(timestamp){
-  var format = undefined,
-    l = timestamp.length;
-
-  switch(l){
-    case 19:
-      format = 'ctime-pre2.4';
-      break;
-    case 23:
-      format = 'ctime';
-      break;
-    case 24:
-      format = 'iso8601-utc';
-      break;
-    case 28:
-      format = 'iso8601-local';
-      break;
+function getTimestampFormat(timestamp) {
+  var format, l = timestamp.length;
+  switch (l) {
+  case 19:
+    format = 'ctime-pre2.4';
+    break;
+  case 23:
+    format = 'ctime';
+    break;
+  case 24:
+    format = 'iso8601-utc';
+    break;
+  case 28:
+    format = 'iso8601-local';
+    break;
   }
   return format;
 }
-
-function errorMessage(msg){
+function errorMessage(msg) {
   var message = msg;
-
-  if(msg.indexOf('mongod instance already running?') > -1){
+  if (msg.indexOf('mongod instance already running?') > -1) {
     message = 'already running';
   }
-
   return new Error(message);
 }
-
-function getEvent(msg){
-  if(msg.indexOf('exception') > -1){
+function getEvent(msg) {
+  if (msg.indexOf('exception') > -1) {
     return {
       name: 'error',
       data: errorMessage(msg)
     };
   }
-
-  if(msg.indexOf('waiting for connections') > -1) {
+  if (msg.indexOf('waiting for connections') > -1) {
     return {
       name: 'ready',
-      data: {port: parseInt(/(\d+)/.exec(msg)[1], 10)}
+      data: { port: parseInt(/(\d+)/.exec(msg)[1], 10) }
     };
   }
-
   return null;
 }
-
-function Entry(data, opts){
+function Entry(data, opts) {
   opts = opts || {};
   data = data || {};
-
   this.timestamp = data.timestamp || new Date();
   this.timestamp_format = getTimestampFormat(this.timestamp);
-
   this.line = data.line;
   this.message = data.message || '';
   this.tokens = data.line ? data.line.split(' ') : [];
   this.thread = data.thread;
-
   this.event = getEvent(data.message);
-
   var match;
   // connection accepted format
-  if((match = regret('connectionAccepted', data.message))){
+  if (match = regret('connectionAccepted', data.message)) {
     this.conn = 'conn' + match.connNum;
-  }
-  else if(data.thread && data.thread.substring(0, 4) === 'conn'){
+  } else if (data.thread && data.thread.substring(0, 4) === 'conn') {
     this.conn = data.thread;
   }
-
-  if(!data.line || !data.thread){
+  if (!data.line || !data.thread) {
     // Message from shell that is not a log entry
     console.error(data.line);
     return;
   }
-
   parseOperation(this);
 }
-
-function parseNamespaceFields(thisObj){
+function parseNamespaceFields(thisObj) {
   thisObj.namespace = thisObj.tokens[thisObj.timestamp.split(' ').length + 2];
-
   var namespaceTokens = thisObj.namespace.split('.');
   thisObj.database = namespaceTokens[0];
   thisObj.collection = namespaceTokens.slice(1).join('.');
-
   var lastToken = namespaceTokens.slice(-1)[0];
   if (lastToken[0] === '$')
     thisObj.index = lastToken.substring(1);
 }
-
-function parseOperation(thisObj){
-    // the operation type comes after the thread
+function parseOperation(thisObj) {
+  // the operation type comes after the thread
   var opTypeIndex = thisObj.tokens.indexOf('[' + thisObj.thread + ']') + 1;
-
-  if(OPS.indexOf(thisObj.tokens[opTypeIndex]) > -1){
+  if (OPS.indexOf(thisObj.tokens[opTypeIndex]) > -1) {
     var lastToken = thisObj.tokens.slice(-1)[0];
     thisObj.duration = parseInt(lastToken.substring(0, lastToken.length - 2));
     thisObj.operation = thisObj.tokens[2];
-
     parseNamespaceFields(thisObj);
     // opTypeIndex + 2 is where the query object should start
     parseObject('sortShape', 'orderby:', thisObj, opTypeIndex + 2, false);
-
-    var tokensIndex = parseObject('query', 'query:', thisObj, opTypeIndex + 2,
-      false);
+    var tokensIndex = parseObject('query', 'query:', thisObj, opTypeIndex + 2, false);
     parseQueryShape(thisObj);
-
-    if(thisObj.queryShape !== undefined)
+    if (thisObj.queryShape !== undefined)
       thisObj.queryPattern = thisObj.namespace + ' ' + thisObj.queryShape;
-
     var token;
-
     for (; tokensIndex < thisObj.tokens.length; tokensIndex++) {
       token = thisObj.tokens[tokensIndex];
       parseOperationStats(thisObj, token);
     }
   }
 }
-
-function parseOperationStats(thisObj, token){
+function parseOperationStats(thisObj, token) {
   var colonIndex = token.search(':');
   var key, intValue;
-
   // parsing operation stat fields
   if (colonIndex >= 1) {
     key = token.substring(0, colonIndex);
     intValue = parseInt(token.substring(colonIndex + 1));
-
     if (!isNaN(intValue))
       thisObj[key] = intValue;
   }
 }
-
-function parseObject(objectName, objectToken, thisObj, tokensIndex,
-    parsedFirstNestedObj){
+function parseObject(objectName, objectToken, thisObj, tokensIndex, parsedFirstNestedObj) {
   if (thisObj.tokens.indexOf(objectToken) === -1)
     return tokensIndex;
-
   debug('parsing object `' + objectName + '`', thisObj.line);
-
   tokensIndex++;
-  parsedFirstNestedObj = (typeof parsedFirstNestedObj !== 'undefined') ?
-    parsedFirstNestedObj : false;
-
+  parsedFirstNestedObj = typeof parsedFirstNestedObj !== 'undefined' ? parsedFirstNestedObj : false;
   var leftParenCount = 0, rightParenCount = 0;
-  var parsingArray = false,
-      parsingRegex = false,
-      parsingSingleQuotedString = false,
-      parsingDoubleQuotedString = false;
+  var parsingArray = false, parsingRegex = false, parsingSingleQuotedString = false, parsingDoubleQuotedString = false;
   var objectStartIndex = tokensIndex;
   var JSONtokens = thisObj.tokens.slice(0);
   var token;
-
   // when the number of left and right parentheses are equal, we've parsed the
   // object
   do {
     token = thisObj.tokens[tokensIndex];
-
-    if (token === undefined) return tokensIndex;
-
+    if (token === undefined)
+      return tokensIndex;
     // rare edge case handling:
     // we have to know if we're tokenzing tokens that are part of a string or
     // regex since we need to be able to tell the difference between when the
@@ -184,128 +138,90 @@ function parseObject(objectName, objectToken, thisObj, tokensIndex,
       if (token.search('\'') >= 0)
         parsingSingleQuotedString = false;
     } else if (parsingDoubleQuotedString) {
-      if (token.search('\"') >= 0)
+      if (token.search('"') >= 0)
         parsingDoubleQuotedString = false;
     } else if (tokenBeginsWrap(token, '/')) {
       parsingRegex = true;
     } else if (tokenBeginsWrap(token, '\'')) {
       parsingSingleQuotedString = true;
-    } else if (tokenBeginsWrap(token, '\"')) {
+    } else if (tokenBeginsWrap(token, '"')) {
       parsingDoubleQuotedString = true;
     } else if (tokenBeginsWrap(token, '[')) {
       parsingArray = true;
-    }
-
-    // handles the nested objectToken, see test cases
-    else if (!parsedFirstNestedObj && token === objectToken &&
-        (leftParenCount - rightParenCount) === 1) {
-
+    }  // handles the nested objectToken, see test cases
+    else if (!parsedFirstNestedObj && token === objectToken && leftParenCount - rightParenCount === 1) {
       return parseObject(objectName, objectToken, thisObj, tokensIndex, true);
-
-    }
-
-    // parenthesis count
+    }  // parenthesis count
     else if (token === '{') {
       leftParenCount++;
     } else if (token === '}' || token === '},') {
       rightParenCount++;
-    } else if (objectToken === 'query:' && token.length > 1 &&
-        token.slice(-1) === ':') {
-      JSONtokens[tokensIndex] = '\"' + token.slice(0, -1) + '\":';
+    } else if (objectToken === 'query:' && token.length > 1 && token.slice(-1) === ':') {
+      JSONtokens[tokensIndex] = '"' + token.slice(0, -1) + '":';
     }
-
     tokensIndex++;
   } while (leftParenCount !== rightParenCount);
-
-  thisObj[objectName] = thisObj.tokens.slice(objectStartIndex, tokensIndex).
-    join(' ');
+  thisObj[objectName] = thisObj.tokens.slice(objectStartIndex, tokensIndex).join(' ');
   if (thisObj[objectName].slice(-2) === '},') {
-    thisObj[objectName] = thisObj[objectName].
-      slice(0, thisObj[objectName].length - 1);
+    thisObj[objectName] = thisObj[objectName].slice(0, thisObj[objectName].length - 1);
   }
-
   if (objectToken === 'query:') {
-    thisObj.queryShape = JSONtokens.slice(objectStartIndex, tokensIndex).
-      join(' ');
+    thisObj.queryShape = JSONtokens.slice(objectStartIndex, tokensIndex).join(' ');
   }
-
   return tokensIndex;
 }
-
-function tokenBeginsWrap(token, wrapSymbol){
-  return token === wrapSymbol ||
-      (token[0] === wrapSymbol &&
-      token.slice(-1) !== wrapSymbol &&
-      token.slice(-2) !== wrapSymbol + ',');
+function tokenBeginsWrap(token, wrapSymbol) {
+  return token === wrapSymbol || token[0] === wrapSymbol && token.slice(-1) !== wrapSymbol && token.slice(-2) !== wrapSymbol + ',';
 }
-
-function parseQueryShape(thisObj){
-  if (thisObj.query === undefined) return;
-
+function parseQueryShape(thisObj) {
+  if (thisObj.query === undefined)
+    return;
   var queryObject;
-  try{
+  try {
     queryObject = JSONL.parse(thisObj.query);
-  }
-  catch(e){
+  } catch (e) {
     debug('Could not parse literal json %s. error: %s', thisObj.query, e);
     return;
   }
-
-  thisObj.queryShape = JSON.stringify(parseQueryShapeObject(queryObject), null, ' ')
-    // remove line breaks
-    .replace(/(\r\n|\n|\r)/gm, ' ')
-    // remove extra spaces
-    .replace(/\s+/g,' ');
+  thisObj.queryShape = JSON.stringify(parseQueryShapeObject(queryObject), null, ' ').replace(/(\r\n|\n|\r)/gm, ' ').replace(/\s+/g, ' ');
 }
-
-function parseQueryShapeObject(obj){
+function parseQueryShapeObject(obj) {
   var value;
-
   for (var key in obj) {
     value = obj[key];
-
-    if(QUERY_OPS.indexOf(key) > -1) return 1;
-
-    if(Array.isArray(value))
+    if (QUERY_OPS.indexOf(key) > -1)
+      return 1;
+    if (Array.isArray(value))
       obj[key] = parseQueryShapeArray(value);
-    else if (typeof(value) === 'object' && !(value instanceof RegExp))
+    else if (typeof value === 'object' && !(value instanceof RegExp))
       obj[key] = parseQueryShapeObject(value);
     else
       obj[key] = 1;
   }
-
   return obj;
 }
-
-function parseQueryShapeArray(ary){
+function parseQueryShapeArray(ary) {
   var ele;
-
   for (var i = 0; i < ary.length; i++) {
     ele = ary[i];
-
     if (Array.isArray(ele))
       parseQueryShapeArray(ele);
-    else if (typeof(ele) === 'object')
+    else if (typeof ele === 'object')
       parseQueryShapeObject(ele);
   }
-
   return ary.sort();
 }
-
-module.exports.parse = function(lines, opts){
+module.exports.parse = function (lines, opts) {
   opts = opts || {};
-  if(!Array.isArray(lines)){
+  if (!Array.isArray(lines)) {
     lines = [lines];
   }
-
-  return lines.filter(function(line){
+  return lines.filter(function (line) {
     return line && line.length > 0;
-  }).map(function(line){
+  }).map(function (line) {
     var match = regret(/^mongodb.log/, line, opts);
-
     if (match === null)
       return { line: line };
-
     return new Entry(match);
   });
 };
--- test/parse.test.js
+++ test/parse.test.js
@@ -1,16 +1,16 @@
 var log = require('./..'),
   assert = require('assert');
 
-describe('parse', function(){
-  it('should match the old format', function(){
+describe('parse', function() {
+  it('should match the old format', function() {
     var expected = {
-      thread: 'initandlisten',
-      message: 'db version v2.5.6-pre-',
-      timestamp: 'Wed Mar 12 14:42:31',
-      event: null
-    },
-    line = 'Wed Mar 12 14:42:31 [initandlisten] db version v2.5.6-pre-',
-    res = log.parse(line)[0];
+        thread: 'initandlisten',
+        message: 'db version v2.5.6-pre-',
+        timestamp: 'Wed Mar 12 14:42:31',
+        event: null
+      },
+      line = 'Wed Mar 12 14:42:31 [initandlisten] db version v2.5.6-pre-',
+      res = log.parse(line)[0];
 
     assert.equal(res.event, expected.event);
     assert.equal(res.timestamp, expected.timestamp);
@@ -18,62 +18,77 @@ describe('parse', function(){
     assert.equal(res.name, expected.name);
   });
 
-  it('should handle fatal errors', function(){
+  it('should handle fatal errors', function() {
     var line = 'Wed Mar 12 14:42:31 [initandlisten] exception in ' +
-      'initAndListen: 10309 Unable to create/open lock file: ' +
-      '/data/mongod.lock errno:13 Permission denied Is a mongod instance ' +
-      'already running?, terminating';
+    'initAndListen: 10309 Unable to create/open lock file: ' +
+    '/data/mongod.lock errno:13 Permission denied Is a mongod instance ' +
+    'already running?, terminating';
     var res = log.parse(line)[0];
     assert.equal(res.event.name, 'error');
   });
 
-  it('should not puke on nulls', function(){
+  it('should not puke on nulls', function() {
     log.parse(null);
   });
 
-  it('should accept multiple lines', function(){
+  it('should accept multiple lines', function() {
     var expected = [
-      { name: 'conn611',
-        message: 'end connection 127.0.0.1:57499 (22 connections now open)',
-        timestamp: '2014-05-16T10:39:00.938-0400',
-        event: null },
-      { name: 'clientcursormon',
-        message: 'mem (MB) res:9 virt:3514',
-        timestamp: '2014-05-16T10:43:24.840-0400',
-        event: null },
-      { name: 'clientcursormon',
-        message: ' mapped (incl journal view):960',
-        timestamp: '2014-05-16T10:43:24.840-0400',
-        event: null },
-      { name: 'clientcursormon',
-        message: ' connections:22',
-        timestamp: '2014-05-16T10:43:24.840-0400',
-        event: null },
-      { name: 'clientcursormon',
-        message: 'mem (MB) res:9 virt:3514',
-        timestamp: '2014-05-16T10:48:24.926-0400',
-        event: null },
-      { name: 'clientcursormon',
-        message: ' mapped (incl journal view):960',
-        timestamp: '2014-05-16T10:48:24.926-0400',
-        event: null },
-      { name: 'clientcursormon',
-        message: ' connections:22',
-        timestamp: '2014-05-16T10:48:24.926-0400',
-        event: null }
-    ],
-    lines = [
-      '2014-05-16T10:39:00.938-0400 [conn611] end connection 127.0.0.1:57499 ' +
-      '(22 connections now open)',
-      '2014-05-16T10:43:24.840-0400 [clientcursormon] mem (MB) res:9 virt:3514',
-      '2014-05-16T10:43:24.840-0400 [clientcursormon]  mapped (incl journal ' +
-      'view):960',
-      '2014-05-16T10:43:24.840-0400 [clientcursormon]  connections:22',
-      '2014-05-16T10:48:24.926-0400 [clientcursormon] mem (MB) res:9 virt:3514',
-      '2014-05-16T10:48:24.926-0400 [clientcursormon]  mapped (incl journal ' +
-      'view):960',
-      '2014-05-16T10:48:24.926-0400 [clientcursormon]  connections:22'
-    ], res = log.parse(lines);
+        {
+          name: 'conn611',
+          message: 'end connection 127.0.0.1:57499 (22 connections now open)',
+          timestamp: '2014-05-16T10:39:00.938-0400',
+          event: null
+        },
+        {
+          name: 'clientcursormon',
+          message: 'mem (MB) res:9 virt:3514',
+          timestamp: '2014-05-16T10:43:24.840-0400',
+          event: null
+        },
+        {
+          name: 'clientcursormon',
+          message: ' mapped (incl journal view):960',
+          timestamp: '2014-05-16T10:43:24.840-0400',
+          event: null
+        },
+        {
+          name: 'clientcursormon',
+          message: ' connections:22',
+          timestamp: '2014-05-16T10:43:24.840-0400',
+          event: null
+        },
+        {
+          name: 'clientcursormon',
+          message: 'mem (MB) res:9 virt:3514',
+          timestamp: '2014-05-16T10:48:24.926-0400',
+          event: null
+        },
+        {
+          name: 'clientcursormon',
+          message: ' mapped (incl journal view):960',
+          timestamp: '2014-05-16T10:48:24.926-0400',
+          event: null
+        },
+        {
+          name: 'clientcursormon',
+          message: ' connections:22',
+          timestamp: '2014-05-16T10:48:24.926-0400',
+          event: null
+        }
+      ],
+      lines = [
+        '2014-05-16T10:39:00.938-0400 [conn611] end connection 127.0.0.1:57499 ' +
+        '(22 connections now open)',
+        '2014-05-16T10:43:24.840-0400 [clientcursormon] mem (MB) res:9 virt:3514',
+        '2014-05-16T10:43:24.840-0400 [clientcursormon]  mapped (incl journal ' +
+        'view):960',
+        '2014-05-16T10:43:24.840-0400 [clientcursormon]  connections:22',
+        '2014-05-16T10:48:24.926-0400 [clientcursormon] mem (MB) res:9 virt:3514',
+        '2014-05-16T10:48:24.926-0400 [clientcursormon]  mapped (incl journal ' +
+        'view):960',
+        '2014-05-16T10:48:24.926-0400 [clientcursormon]  connections:22'
+      ],
+      res = log.parse(lines);
 
     for (var i = 0; i < expected.length; i++) {
       assert.equal(res.event, expected.event);
@@ -83,26 +98,33 @@ describe('parse', function(){
     }
   });
 
-  it('should grok the ready event', function(){
+  it('should grok the ready event', function() {
     var expected = [
-      { thread: 'initandlisten',
-        message: 'recover : no journal files present, no recovery needed',
-        timestamp: '2014-05-16T10:50:13.450-0400',
-        event: null
-      },
-      {
-        thread: 'initandlisten',
-        message: 'waiting for connections on port 27017',
-        timestamp: '2014-05-16T10:50:13.579-0400',
-        event: { name: 'ready', data: { port: 27017 } }
-      }
-    ],
-    lines = [
-      '2014-05-16T10:50:13.450-0400 [initandlisten] recover : no journal ' +
-      'files present, no recovery needed',
-      '2014-05-16T10:50:13.579-0400 [initandlisten] waiting for connections ' +
-      'on port 27017'
-    ], res = log.parse(lines);
+        {
+          thread: 'initandlisten',
+          message: 'recover : no journal files present, no recovery needed',
+          timestamp: '2014-05-16T10:50:13.450-0400',
+          event: null
+        },
+        {
+          thread: 'initandlisten',
+          message: 'waiting for connections on port 27017',
+          timestamp: '2014-05-16T10:50:13.579-0400',
+          event: {
+            name: 'ready',
+            data: {
+              port: 27017
+            }
+          }
+        }
+      ],
+      lines = [
+        '2014-05-16T10:50:13.450-0400 [initandlisten] recover : no journal ' +
+        'files present, no recovery needed',
+        '2014-05-16T10:50:13.579-0400 [initandlisten] waiting for connections ' +
+        'on port 27017'
+      ],
+      res = log.parse(lines);
 
     for (var i = 0; i < expected.length; i++) {
       assert.equal(res.event, expected.event);
