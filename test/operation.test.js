ecb57a302d56f21e89a4befb15c5a4907eb39f67
Waley Chen <waleycz@gmail.com>
2014-08-12 13:26:17 -0400

    rewrite object parsing to make it way simpler; also added comment and $query parsing

--- test/operation.test.js
+++ test/operation.test.js
@@ -1,7 +1,25 @@
 var assert = require('assert'),
-  log = require('./..');
+    log = require('./..');
 
-describe('Opererations', function() {
+describe('Operations', function() {
+  // comment field
+  it('should parse the comment field', function() {
+    lines = [
+      '2014-08-11T12:00:30.653-0400 [conn18114] query admin.system' +
+      '.namespaces query: { $query: {}, $comment: { mongoscope_feature: "get' + 
+      ' instance collections" } } planSummary: COLLSCAN ntoreturn:0 ntoskip:0' + 
+      ' nscanned:1 nscannedObjects:1 keyUpdates:0 numYields:0 locks(micros) ' + 
+      'r:93 nreturned:1 reslen:89 0ms'
+    ];
+    expectedComments = [
+      '{ \"mongoscope_feature\": "get instance collections" }'
+    ];
+    res = log.parse(lines);
+
+    for (var i = 0; i < lines.length; i++)
+      assert.equal(res[i].comment, expectedComments[i]);
+  }),
+
   // namespace fields
   it('should parse the namespace field and it\'s related fields', function() {
     var namespaceFields = {
@@ -53,57 +71,57 @@ describe('Opererations', function() {
   // operation type and stats
   it('should parse the query operation type and stats', function() {
     var lines = [
-        '2014-06-02T14:26:48.300-0400 [initandlisten] command admin.system ' +
-        'planSummary: EOF ntoreturn:0 ntoskip:0 nscanned:0 nscannedObjects:0 ' +
-        'keyUpdates:0 numYields:0 locks(micros) W:2347 r:243 nreturned:30000 ' +
-        'reslen:20 nmoved:11 ninserted:100 900000ms',
-        '2014-06-02T14:27:48.300-0400 [TTLMonitor] query admin.system.indexes ' +
-        'query: { expireAfterSeconds: { $exists: true } } planSummary: EOF ' +
-        'ntoreturn:9 ntoskip:9 nscanned:99 nscannedObjects:0 keyUpdates:9001 ' +
-        'numYields:9999 locks(micros) w:1111 R:568 nreturned:0 reslen:20 ' +
-        'nmoved:11 ndeleted:100 nupdated:1000 0ms'
-      ],
-      expected = [
-        {
-          W: 2347,
-          collection: 'system',
-          database: 'admin',
-          duration: 900000,
-          keyUpdates: 0,
-          namespace: 'admin.system',
-          ninserted: 100,
-          nmoved: 11,
-          nreturned: 30000,
-          nscanned: 0,
-          nscannedObjects: 0,
-          ntoskip: 0,
-          ntoreturn: 0,
-          numYields: 0,
-          operation: 'command',
-          r: 243,
-          reslen: 20
-        },
-        {
-          R: 568,
-          collection: 'system.indexes',
-          database: 'admin',
-          duration: 0,
-          keyUpdates: 9001,
-          namespace: 'admin.system.indexes',
-          ndeleted: 100,
-          nmoved: 11,
-          nreturned: 0,
-          nscanned: 99,
-          nscannedObjects: 0,
-          ntoskip: 9,
-          ntoreturn: 9,
-          nupdated: 1000,
-          numYields: 9999,
-          operation: 'query',
-          reslen: 20,
-          w: 1111
-        }
-      ];
+      '2014-06-02T14:26:48.300-0400 [initandlisten] command admin.system ' +
+      'planSummary: EOF ntoreturn:0 ntoskip:0 nscanned:0 nscannedObjects:0 ' +
+      'keyUpdates:0 numYields:0 locks(micros) W:2347 r:243 nreturned:30000 ' +
+      'reslen:20 nmoved:11 ninserted:100 900000ms',
+      '2014-06-02T14:27:48.300-0400 [TTLMonitor] query admin.system.indexes ' +
+      'query: { expireAfterSeconds: { $exists: true } } planSummary: EOF ' +
+      'ntoreturn:9 ntoskip:9 nscanned:99 nscannedObjects:0 keyUpdates:9001 ' +
+      'numYields:9999 locks(micros) w:1111 R:568 nreturned:0 reslen:20 ' +
+      'nmoved:11 ndeleted:100 nupdated:1000 0ms'
+    ],
+    expected = [
+      {
+        W: 2347,
+        collection: 'system',
+        database: 'admin',
+        duration: 900000,
+        keyUpdates: 0,
+        namespace: 'admin.system',
+        ninserted: 100,
+        nmoved: 11,
+        nreturned: 30000,
+        nscanned: 0,
+        nscannedObjects: 0,
+        ntoskip: 0,
+        ntoreturn: 0,
+        numYields: 0,
+        operation: 'command',
+        r: 243,
+        reslen: 20
+      },
+      {
+        R: 568,
+        collection: 'system.indexes',
+        database: 'admin',
+        duration: 0,
+        keyUpdates: 9001,
+        namespace: 'admin.system.indexes',
+        ndeleted: 100,
+        nmoved: 11,
+        nreturned: 0,
+        nscanned: 99,
+        nscannedObjects: 0,
+        ntoskip: 9,
+        ntoreturn: 9,
+        nupdated: 1000,
+        numYields: 9999,
+        operation: 'query',
+        reslen: 20,
+        w: 1111
+      }
+    ];
     res = log.parse(lines);
 
     for (var i = 0; i < lines.length; i++)
@@ -115,42 +133,45 @@ describe('Opererations', function() {
   // query field
   it('should parse the query field or nested query field', function() {
     var queries = [
-        '{}',
-        '{ x: 20.0 }',
-        '{ field1: 1 }',
-        '{ field1: [ 1 ] }',
-        '{ query: {}, orderby: { age: -1.0 } }',
-        '{ field1: { field2: { field3: \'val3\' }, field4: \'val4\' }, ' +
-        'field5: \'val5\' }',
-        '{ field1: { field2: { query: \'val3\' }, field4: \'val4\' }, ' +
-        'field5: \'val5\' }',
-        '{ field1: { query: { query: \'val3\' }, query: \'val4\' }, ' +
-        'query: { query: { query: \'val3\' }, field4: \'val4\' } }',
-        '{ field1: /regex/ }',
-        '{ field1: /regex { query: regex/ }',
-        '{ field: /wefwef " query: acme.*corp/i }',
-        '{ field1: / { query: } /, query: { query: \' / val3 / aaa\' }, x: 1 }',
-        '{ field1: \'blah \" query: \" query: blah\' }',
-        '{ field1: [ \'a query: a\' ] }'
-      ],
-      expectedQueries = [
-        '{}',
-        '{ x: 20.0 }',
-        '{ field1: 1 }',
-        '{ field1: [ 1 ] }',
-        '{}',
-        '{ field1: { field2: { field3: \'val3\' }, field4: \'val4\' }, ' +
-        'field5: \'val5\' }',
-        '{ field1: { field2: { query: \'val3\' }, field4: \'val4\' }, ' +
-        'field5: \'val5\' }',
-        '{ query: { query: \'val3\' }, field4: \'val4\' }',
-        '{ field1: /regex/ }',
-        '{ field1: /regex { query: regex/ }',
-        '{ field: /wefwef " query: acme.*corp/i }',
-        '{ query: \' / val3 / aaa\' }',
-        '{ field1: \'blah \" query: \" query: blah\' }',
-        '{ field1: [ \'a query: a\' ] }'
-      ];
+      '{}',
+      '{ x: 20.0 }',
+      '{ field1: 1 }',
+      '{ field1: [ 1 ] }',
+      '{ query: {}, orderby: { age: -1.0 } }',
+      '{ field1: { field2: { field3: \'val3\' }, field4: \'val4\' }, ' +
+      'field5: \'val5\' }',
+      '{ field1: { field2: { query: \'val3\' }, field4: \'val4\' }, ' +
+      'field5: \'val5\' }',
+      '{ field1: { query: { query: \'val3\' }, query: \'val4\' }, ' +
+      'query: { query: { query: \'val3\' }, field4: \'val4\' } }',
+      '{ field1: /regex/ }',
+      '{ field1: /regex/, field2: { query: /regex/ } }',
+      '{ field: /wefwef " query: acme.*corp/i }',
+      '{ field1: / { query: } /, query: { query: \' / val3 / aaa\' }, x: 1 }',
+      '{ field1: \'blah \" query: \" query: blah\' }',
+      '{ field1: [ \'a query: a\' ] }',
+      '{ $query: {}, $comment: { mongoscope_feature: "get instance collection' + 
+      's" } }'
+    ],
+    expectedQueries = [
+      '{}',
+      '{ x: 20.0 }',
+      '{ field1: 1 }',
+      '{ field1: [ 1 ] }',
+      '{}',
+      '{ field1: { field2: { field3: \'val3\' }, field4: \'val4\' }, ' +
+      'field5: \'val5\' }',
+      '{ field1: { field2: { query: \'val3\' }, field4: \'val4\' }, ' +
+      'field5: \'val5\' }',
+      '{ \"query\": { \"query\": \"val3\" }, \"field4\": \"val4\" }',
+      '{ field1: /regex/ }',
+      '{ field1: /regex/, field2: { query: /regex/ } }',
+      '{ field: /wefwef " query: acme.*corp/i }',
+      '{ \"query\": \" / val3 / aaa\" }',
+      '{ field1: \'blah \" query: \" query: blah\' }',
+      '{ field1: [ \'a query: a\' ] }',
+      '{}'
+    ];
 
     var line, res;
 
@@ -183,22 +204,22 @@ describe('Opererations', function() {
   // query shape field
   it('should parse the query shape', function() {
     var queries = [
-        '{ x: 20.0 }',
-        '{ field: { $exists: true } }',
-        '{ f11: true, f22: 55, f33: \'str\', $f44: / regex $gt / }',
-        '{ f1: [ 3, 2, 1 ] }',
-        '{ orderby: { x123: 1.0 }, field1: / { orderby: } /, query: { query: \'' +
-        ' / val3 / aaa\' } }',
-        '{ expireAfterSeconds: { $exists: 1 } }'
-      ],
-      expectedQueryShapes = [
-        '{ \"x\": 1 }',
-        '{ \"field\": 1 }',
-        '{ \"f11\": 1, \"f22\": 1, \"f33\": 1, \"$f44\": 1 }',
-        '{ \"f1\": [ 1, 2, 3 ] }',
-        '{ \"query\": 1 }',
-        '{ \"expireAfterSeconds\": 1 }'
-      ];
+      '{ x: 20.0 }',
+      '{ field: { $exists: true } }',
+      '{ f11: true, f22: 55, f33: \'str\', $f44: / regex $gt / }',
+      '{ f1: [ 3, 2, 1 ] }',
+      '{ orderby: { x123: 1.0 }, field1: / { orderby: } /, query: { query: \'' +
+      ' / val3 / aaa\' } }',
+      '{ expireAfterSeconds: { $exists: 1 } }'
+    ],
+    expectedQueryShapes = [
+      '{ \"x\": 1 }',
+      '{ \"field\": 1 }',
+      '{ \"f11\": 1, \"f22\": 1, \"f33\": 1, \"$f44\": 1 }',
+      '{ \"f1\": [ 1, 2, 3 ] }',
+      '{ \"query\": 1 }',
+      '{ \"expireAfterSeconds\": 1 }'
+    ];
 
     var line, res;
 
@@ -217,18 +238,18 @@ describe('Opererations', function() {
   // sort shape field
   it('should parse the sort shape field', function() {
     var queries = [
-        '{ orderby: { x123: 1.0 }, field1: / { orderby: } /, query: { query: \'' +
-        ' / val3 / aaa\' } }',
-        '{ field1: / { orderby: } /, query: { orderby: \' / val3 / aaa\' }, ' +
-        'orderby: { x123: -1.0 } }',
-        '{ field1: / { orderby: } /, query: { orderby: \' / orderby / aaa\' }, ' +
-        'test: \'wee\', orderby: { x123: -1.0 } }'
-      ],
-      expectedSortShapes = [
-        '{ x123: 1.0 }',
-        '{ x123: -1.0 }',
-        '{ x123: -1.0 }'
-      ]
+      '{ orderby: { x123: 1.0 }, field1: / { orderby: } /, query: { query: \'' +
+      ' / val3 / aaa\' } }',
+      '{ field1: / { orderby: } /, query: { orderby: \' / val3 / aaa\' }, ' +
+      'orderby: { x123: -1.0 } }',
+      '{ field1: / { orderby: } /, query: { orderby: \' / orderby / aaa\' }, ' +
+      'test: \'wee\', orderby: { x123: -1.0 } }'
+    ],
+    expectedSortShapes = [
+      '{ \"x123\": 1 }',
+      '{ \"x123\": -1 }',
+      '{ \"x123\": -1 }'
+    ]
 
     var line, res;
     for (var i = 0; i < queries.length; i++) {
