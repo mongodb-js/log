78903d3d9dd10c5f885f09438d8f24351fb7ecd6
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-19 18:36:03 -0400

    progress on map reduce support

---
+++ test/mapreduce.test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var parseFixture = require('./helpers').parseFixture;
+
+describe('mapreduce', function() {
+  it('should work', function(done) {
+    parseFixture('mapreduce', function(err, lines) {
+      if (err) return done(err);
+      console.log('lines', JSON.stringify(lines, null, 2));
+
+      var res = lines[0];
+      assert.equal(res.operation, 'mapreduce');
+      done();
+    });
+  });
+});
